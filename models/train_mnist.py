3d3e647fdbebea29141400681a4e8fe5a886f90a
Vishwa Karia <45134824+vishwakaria@users.noreply.github.com>
2023-10-19 15:22:32 -0700

    Update train_mnist.py to add signal handler

--- models/train_mnist.py
+++ models/train_mnist.py
@@ -1,125 +1,141 @@
-import torch as T
-from torch import nn
-import torch.nn.functional as F
-from torchvision import datasets, transforms
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import signal
 import sys
-import numpy as np
-import copy as cp
+import os
+import time
 
-device = T.device("cuda" if T.cuda.is_available() else "cpu")
+# Define global variables for the model, optimizer, and other checkpoint-related data
+net = None
+optimizer = None
+checkpoint_path = '/mnt/shared/checkpoints/model.pt'
+epoch = 0
+sig_received = False
 
-def acc(log_ps, labels):
-    ps = T.exp(log_ps)
-    _, top_class = ps.topk(1, dim=1)
-    equals = top_class == labels.view(*top_class.shape)
-    accuracy = equals.to(T.float).mean()
-
-    return accuracy
-
-def data():
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((.5,), (.5,))
-    ])
-
-    trainset = datasets.MNIST(".pytorch/MNIST", download=True, train=True, transform=transform)
-    valset = datasets.MNIST(".pytorch/MNIST", download=True, train=False, transform=transform)
-    trainloader = T.utils.data.DataLoader(trainset, batch_size=128, shuffle=True)
-    valloader = T.utils.data.DataLoader(valset, batch_size=128, shuffle=True)
-
-    return trainloader, valloader
-
-def train(mdl, data_fn=data, epochs=2, alpha=1e-3):
-    optim = T.optim.Adam(mdl.parameters(), lr=alpha)
-    criterion = nn.NLLLoss()
-    trainloader, valloader = data_fn()
-
-    best_val_so_far = np.inf
-    best_model = None
-    accs = []
-    losses = []
-    val_accs = []
-    val_losses = []
-
-    print("Started parameter optimization")
-    for e in range(1, epochs + 1):
-        running_acc = 0
-        running_loss = 0
-        val_running_acc = 0
-        val_running_loss = 0
-
-        # Training
-        mdl.train()
-        for imgs, lbls in trainloader:
-            imgs = imgs.to(device)
-            lbls = lbls.to(device)
-
-            log_y_hat = mdl(imgs)
-            loss = criterion(log_y_hat, lbls)
-
-            # Parameter optimization
-            optim.zero_grad()
-            loss.backward()
-            optim.step()
-
-            running_acc += acc(log_y_hat, lbls)
-            running_loss += loss.item()
-        else:
-            # Validation
-            mdl.eval()
-            with T.no_grad():
-                for imgs, lbls in valloader:
-                    imgs = imgs.to(device)
-                    lbls = lbls.to(device)
-
-                    val_log_y_hat = mdl(imgs)
-                    val_loss = criterion(val_log_y_hat, lbls)
-
-                    val_running_acc += acc(val_log_y_hat, lbls)
-                    val_running_loss += val_loss
-
-        accs.append(running_acc/len(trainloader))
-        losses.append(running_loss/len(trainloader))
-        val_accs.append(val_running_acc/len(valloader))
-        val_losses.append(val_running_loss/len(valloader))
-
-        print("Epoch {:2} | acc: {:5.4f} loss: {:5.4f} | val_acc: {:5.4f} val_loss: {:5.4f}".format(
-            e,
-            accs[-1],
-            losses[-1],
-            val_accs[-1],
-            val_losses[-1]
-        ), end="")
-
-        # Best Model
-        if val_running_loss/len(valloader) < best_val_so_far:
-            best_model = cp.deepcopy(mdl)
-            best_val_so_far = val_running_loss/len(valloader)
-            print(" *")
-        else:
-            print("")
-
-    best_model.train()
-    return best_model, (accs, losses, val_accs, val_losses)
-
-class NN(nn.Module):
+# Define a custom neural network
+class CustomNN(nn.Module):
     def __init__(self):
-        super().__init__()
-
-        self.fc1 = nn.Linear(784, 256)
-        self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, 64)
-        self.out = nn.Linear(64, 10)
+        super(CustomNN, self).__init__()
+        self.fc1 = nn.Linear(784, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 10)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x.view(-1, 784)))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = F.log_softmax(self.out(x), dim=1)
+        x = x.view(x.size(0), -1)
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
-        return x.view(-1, 10)
+# Define the training function
+def train(trainloader, criterion, device, epochs):
+    global net
+    global optimizer
+    global epoch
 
-print("training begins..")
-model = NN()
-model = model.to(device)
-model, metrics = train(model)
+    if os.path.exists(checkpoint_path):
+        checkpoint = torch.load(checkpoint_path)
+        net.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        start_epoch = checkpoint['epoch'] + 1
+    else:
+        start_epoch = 0
+
+    net.to(device)
+    net.train()
+
+    for e in range(start_epoch, epochs):
+        running_loss = 0.0
+        epoch = e
+        if sig_received == True:
+            checkpoint_and_exit()
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data[0].to(device), data[1].to(device)
+
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()            
+        print(f'Epoch {epoch + 1}, Loss: {running_loss / (i + 1)}')
+
+    print('Finished Training')
+
+"""
+# Define a function to ckpt
+def checkpoint_and_exit():
+    global checkpoint_path
+    global net
+    global optimizer
+    print(f"epoch: {epoch}")
+    print(f"Checkpoint Path: {checkpoint_path}")
+    print(f"net: {net}") if net is not None else None
+    print(f"optimizer: {optimizer}") if optimizer is not None else None
+    if os.path.exists(checkpoint_path) and net is not None and optimizer is not None:
+        print("inside ckpt")
+        checkpoint = {
+            'epoch': epoch,
+            'model_state_dict': net.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+        }
+        try:
+            torch.save(checkpoint, checkpoint_path)
+            print("Done saving ckpt")
+        except Exception as e:
+            print(f"Error saving ckpt: {e}")
+    sys.exit(0)
+
+"""
+def sigterm_handler(signum, frame):
+    global sig_received
+    print(f"{signum} caught, setting flag")
+    sig_received = True
+
+# Mock checkpoint method to measure time allowed for ckpt
+def checkpoint_and_exit():
+    try:
+        print(" saving checkpoint now")
+        start = time.time()
+        end = start + 120
+        i = 0
+        while time.time() < end:
+            time.sleep(2)
+            print(f"Counter: {i}")
+            i += 1
+        print("done")
+        sys.exit(0)
+    except Exception as e:
+        print(f"Error saving ckpt: {e}")
+
+
+# Main script
+if __name__ == "__main__":
+    # Set device to GPU if available, else CPU
+    print('start training')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Define data transformations and load the MNIST dataset
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+    trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+    # Create the custom neural network
+    net = CustomNN()
+
+    # Define loss function and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    epochs = 100
+    
+    # Set the SIGTERM signal handler
+    signal.signal(signal.SIGUSR1, sigterm_handler)
+    signal.signal(signal.SIGTERM, sigterm_handler)
+
+    # Train the model, optionally starting from a checkpoint
+    train(trainloader, criterion, device, epochs)
+    print('done training')
