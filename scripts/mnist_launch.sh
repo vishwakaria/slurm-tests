74000fc89dfeb4b98519e91d47c7951e20d3aa8f
Vishwa Karia <45134824+vishwakaria@users.noreply.github.com>
2023-11-03 17:02:24 -0700

    Update mnist_launch.sh to use IP address in master_addr

--- scripts/mnist_launch.sh
+++ scripts/mnist_launch.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+  
 # Enable conda environment
 eval "$(conda shell.bash hook)"
-conda activate /mnt/shared/myenv
+conda activate /fsx/myenv
 
 NODE_LIST=$(scontrol show jobid=$SLURM_JOBID | awk -F= '/NodeList=/{print $2}' | grep -v Exc)
-MASTER_ADDR=$(scontrol show hostname | head -n 1)
+MASTER_NODE=$(scontrol show hostname $NODE_LIST | head -n 1)
+MASTER_ADDR=$(scontrol show node=$MASTER_NODE | awk -F= '/NodeAddr=/{print $2}' | awk '{print $1}')
 
 echo $NODE_LIST
 echo $SLURM_NNODES
@@ -15,4 +16,4 @@ torchrun --nnodes=$SLURM_NNODES \
          --node_rank=$SLURM_NODEID \
          --master-addr=$MASTER_ADDR \
          --master_port=1234 \
-         /mnt/shared/models/train_mnist.py
+         /fsx/models/train_mnist.py
